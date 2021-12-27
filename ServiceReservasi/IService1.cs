5b12d541bbbead765f2a8a42618ea7a23429d7ba
Seliesca <seliescawhidyapermata8289@gmail.com>
2021-12-27 23:52:37 +0700

    praktikum 11

--- ServiceReservasi/IService1.cs
+++ ServiceReservasi/IService1.cs
@@ -12,17 +12,53 @@ namespace ServiceReservasi
     public interface IService1
     {
         [OperationContract]
+        string Login(string username, string password);
+
+        [OperationContract]
+        string Register(string username, string password, string kategori);
+
+        [OperationContract]
+        string UpdateRegister(string username, string password, string kategori, int id);
+
+        [OperationContract]
+        string DeleteRegister(string username);
+
+        [OperationContract]
+        List<DataRegister> DataRegist();
+
+        [OperationContract]
         string pemesanan(string IDPemesanan, string NamaCustomer, string NoTelpon, int JumlahPemesanan, string IDLokasi); //method //proses input data
+
         [OperationContract]
         string editPemesanan(string IDPemesanan, string Namacustomer, string No_telpon);
+
         [OperationContract]
         string deletePemesanan(string IDPemesanan);
+
         [OperationContract]
-        List<CekLokasi> ReviewLokasi();
+        List<CekLokasi> ReviewLokasi(); //nampilin data yg ada di database //menampilkan isi dari yang ada contract
+
         [OperationContract]
         List<DetailLokasi> DetailLokasi();
+
         [OperationContract]
         List<Pemesanan> Pemesanan();
+
+        // TODO: Add your service operations here
+    }
+
+    [DataContract]
+    public class DataRegister
+    {
+        [DataMember(Order = 1)]
+        public int id { get; set; }
+        [DataMember(Order = 2)]
+        public string username { get; set; }
+        [DataMember(Order = 3)]
+        public string password { get; set; }
+        [DataMember(Order = 4)]
+        public string kategori { get; set; }
+
     }
 
     [DataContract]
@@ -68,6 +104,7 @@ namespace ServiceReservasi
         public string Lokasi { get; set; }
 
     }
+
 
     // Use a data contract as illustrated in the sample below to add composite types to service operations.
     // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "ServiceReservasi.ContractType".
